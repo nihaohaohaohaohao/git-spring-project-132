e3e1f873e2b41a4ff0cc04e0c1ca3f41bd6ba8aa
johnny6865 <2509529997@qq.com>
2022-01-19 16:55:46 +0800

    第一次修改springDamo.java

--- spring-maven-project/src/main/java/com/intouch/spring/SpringDemo.java
+++ spring-maven-project/src/main/java/com/intouch/spring/SpringDemo.java
@@ -11,7 +11,8 @@ public class SpringDemo {
     public static void main(String[] args) {
         //创建ioc容器
         ApplicationContext  context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        Date now=context.getBean(Date.class);
+        Date now=(Date) context.getBean("now");
+        //Date now=context.getBean(Date.class);
         DateFormat dateFormat =context.getBean(SimpleDateFormat.class);
         System.out.println("现在的时间是"+dateFormat.format(now));}
 }
