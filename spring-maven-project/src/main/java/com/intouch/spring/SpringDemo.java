2ed0fe5205633410404a3d2123747b1890756906
johnny6865 <2509529997@qq.com>
2022-01-19 16:50:25 +0800

    创建spring-maven-project

---
+++ spring-maven-project/src/main/java/com/intouch/spring/SpringDemo.java
@@ -0,0 +1,17 @@
+package com.intouch.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SpringDemo {
+    public static void main(String[] args) {
+        //创建ioc容器
+        ApplicationContext  context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        Date now=context.getBean(Date.class);
+        DateFormat dateFormat =context.getBean(SimpleDateFormat.class);
+        System.out.println("现在的时间是"+dateFormat.format(now));}
+}
